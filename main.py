dd7b1bcb3bde06e28f1f3fdf0bc04c09abb6a758
RF Fahad Islam <riyad9949@gmail.com>
2025-10-08 16:10:47 +0600

    file streaming

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI,Request,HTTPException
-from fastapi.responses import FileResponse,RedirectResponse
+from fastapi import FastAPI,Request
+from fastapi.responses import StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
@@ -7,19 +7,16 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 from docxtpl import DocxTemplate
 from datetime import datetime
-import tempfile
-from typing import Annotated
+from io import BytesIO
 
 def generateInvoice(datas:dict):
     template = os.path.join(FILE_DIRECTORY, "invoice_template.docx")
     doc = DocxTemplate(template)
     doc.render(datas)
-    # Create a temporary file to save the generated DOCX
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".docx") as tmp:
-        file_name = f"Invoice-{datetime.now().strftime('%d_%m_%Y %H-%M-%S')}.docx"
-        doc.save(os.path.join(FILE_DIRECTORY_2,file_name))
-
-    return file_name
+    fileio = BytesIO()
+    doc.save(fileio)
+    fileio.seek(0)
+    return fileio
     
 class Item(BaseModel):
     item_name: str
@@ -40,7 +37,7 @@ class Invoice(BaseModel):
     
 app = FastAPI()
 FILE_DIRECTORY = "files"
-FILE_DIRECTORY_2 = "genfiles"
+
 app.add_middleware(CORSMiddleware,   allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
@@ -58,22 +55,10 @@ async def home(request: Request):
     )
 @app.post("/invoice")
 async def invoice_generator(invoice:Invoice):
-    file_name = generateInvoice(invoice.model_dump())
-    return {"file_name":file_name}
-
-
-
-@app.get("/download/{filename}")
-async def download_file(filename: str):
-    file_path = os.path.join(FILE_DIRECTORY_2, filename)
-
-    if not os.path.exists(file_path):
-        return {"error": "File not found!"}
-
-    return FileResponse(
-        file_path,
-        media_type="application/octet-stream"  # Generic binary data
+    file_io = generateInvoice(invoice.model_dump())
+    filename = f"Invoice-{datetime.now().strftime('%d_%m_%Y %H-%M-%S')}.docx"
+    return StreamingResponse(
+        file_io,
+        media_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        headers={"Content-Disposition": f"attachment; filename={filename}","filename": filename}
     )
-
-def removejunk(file_name):
-    os.remove(os.path.join(FILE_DIRECTORY_2,file_name))
